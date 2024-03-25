4a75aebaf90874f15c8279922c99f931c0be80c3
Okraila <o.krailevskyi@gmail.com>
2024-03-25 13:40:41 +0200

    quality

--- index.js
+++ index.js
@@ -2,29 +2,39 @@ let menuBtn = document.querySelector(".menu__btn");
 let link = document.querySelector(".header__link");
 let list = document.querySelector(".header__list");
 
-console.log(menuBtn);
-
 menuBtn.addEventListener("click", () => {
   menuBtn.classList.toggle("active");
   list.classList.toggle("active");
 });
 
-const options = {
-  root:null,
-  rootMargin:"0px",
-  treshhold:0.1,
-}
-function callback (entries,observer){
-  entries.forEach(entry => {
-    if (entry.Intersection) {
-      console.log("Елемент Видимий");
-    }
-    else{
-      console.log("Не Видимий Елемент");
-    }
-  });
-}
-const observer = new IntersectionObserver(callback,options)
-const targetElement = document.querySelector(".customers__list")
-console.log(targetElement);
-observer.observe(targetElement)
+const isElementInViewport = (el) => {
+  const rect = el.getBoundingClientRect();
+  return (
+    rect.top >= 0 &&
+    rect.left >= 0 &&
+    rect.bottom <=
+      (window.innerHeight || document.documentElement.clientHeight) &&
+    rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+  );
+};
+
+const customersScroll = () => {
+  const section = document.querySelector(".customers__list");
+  if (isElementInViewport(section)) {
+    section.classList.add("customers__animation");
+    window.removeEventListener("scroll", customersScroll);
+  }
+};
+
+window.addEventListener("scroll", customersScroll);
+
+const qualityScroll = () => {
+  const section = document.querySelector(".quality__inner");
+  if (isElementInViewport(section)) {
+    section.classList.add("quality__animation");
+    window.removeEventListener("scroll", qualityScroll);
+  }
+};
+
+window.addEventListener("scroll", qualityScroll);
+
