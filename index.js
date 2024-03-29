48b9a77b7618c0621de8764bdb728d825fdb0ee0
Okraila <o.krailevskyi@gmail.com>
2024-03-29 20:34:55 +0200

    quality

--- index.js
+++ index.js
@@ -7,35 +7,35 @@ menuBtn.addEventListener("click", () => {
   list.classList.toggle("active");
 });
 
-const isElementInViewport = (el) => {
-  const rect = el.getBoundingClientRect();
-  return (
-    rect.top >= 0 &&
-    rect.left >= 0 &&
-    rect.bottom <=
-      (window.innerHeight || document.documentElement.clientHeight) &&
-    rect.right <= (window.innerWidth || document.documentElement.clientWidth)
-  );
-};
+const isElementInViewport = (el)=> {
+  const rect = el.getBoundingClientRect()
+  return(
+    rect.top >=0 &&
+    rect.left >=0 &&
+    rect.bottom <= (window.innerHeight ||document.documentElement.clientHeight ) &&
+    rect.right <= (window.innerWidth ||document.documentElement.clientWidth) 
+  )
+}
 
-const customersScroll = () => {
-  const section = document.querySelector(".customers__list");
+
+const customersScroll = ()=> {
+  const section = document.querySelector(".customers__list")
   if (isElementInViewport(section)) {
-    section.classList.add("customers__animation");
-    window.removeEventListener("scroll", customersScroll);
+    section.classList.add("customers__animation")
+    window.removeEventListener("scroll",customersScroll)
   }
-};
 
-window.addEventListener("scroll", customersScroll);
-customersScroll()
+}
 
-const qualityScroll = () => {
-  const section = document.querySelector(".quality__inner");
+window.addEventListener("scroll",customersScroll)
+
+const qualityScroll = ()=> {
+  const section = document.querySelector(".quality__inner")
   if (isElementInViewport(section)) {
-    section.classList.add("quality__animation");
-    window.removeEventListener("scroll", qualityScroll);
+    section.classList.add("quality__animation")
+    window.removeEventListener("scroll",qualityScroll)
   }
-};
 
-window.addEventListener("scroll", qualityScroll);
-qualityScroll()
+}
+
+window.addEventListener("scroll",qualityScroll)
