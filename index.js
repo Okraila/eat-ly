3d1f54cae9ca82fa168392265253158e94942c9a
Okraila <o.krailevskyi@gmail.com>
2024-03-30 19:41:36 +0200

    restaurants

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 let menuBtn = document.querySelector(".menu__btn");
 let link = document.querySelector(".header__link");
 let list = document.querySelector(".header__list");
+let restaurantsList = document.querySelector(".restaurants__list")
 
 menuBtn.addEventListener("click", () => {
   menuBtn.classList.toggle("active");
@@ -39,3 +40,56 @@ const qualityScroll = ()=> {
 }
 
 window.addEventListener("scroll",qualityScroll)
+
+const restaurantsData = [
+  {
+    id:1,
+    imageUrl:"./images/restaurantimage1",
+    title:"The Chicken King",
+    deliveryTime:24,
+    rating:4.8,
+    status:"Healthy"
+  },
+  {
+    id:2,
+    imageUrl:"./images/restaurantimage2",
+    title:"The Burger King",
+    deliveryTime:24,
+    rating:4.9,
+    status:"Trending"
+  },
+  {
+    id:3,
+    imageUrl:"./images/restaurantimage1",
+    title:"The Chicken King",
+    deliveryTime:24,
+    rating:4.8,
+    status:"Healthy"
+  }
+]
+
+const createRestaurantList = restaurantsData.map((item) => {
+  console.log(item, "item"); 
+  const{id, imageUrl, title, deliveryTime, rating, status} = item;
+  return `
+  <li class="restaurants__item">
+  <img src=${imageUrl}" alt="">
+  <h3 class="restaurants__h3">
+  ${title}
+  </h3>
+  <span class="restaurants__span">
+  ${deliveryTime}
+  </span>
+  <span class="restaurants__span">
+  ${rating}
+  </span>
+  <span class="restaurants__status">
+  ${status}  
+  </span>
+</li>
+  `
+})
+
+.join("");
+restaurantsList.innerHTML = createRestaurantList;
+createRestaurantList();
